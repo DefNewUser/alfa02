9e655c700b8ea62c794cb480a0c139568aa832d2
DefNewUser <def_new_acc@protonmail.com>
2022-06-21 01:01:58 +0300

    added second page

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def index(request):
+#     return HttpResponse('<h1>first text</h1>')
+
 def index(request):
-    return HttpResponse('<h1>first text</h1>')
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
